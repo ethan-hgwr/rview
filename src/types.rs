e7bfaab2b9c91d12daf80da3da50492d45a3651c
ethan <ethansoya@gmail.com>
2026-06-04 02:13:10 +0200

    fix: use correct visibility level

--- src/types.rs
+++ src/types.rs
@@ -1,6 +1,6 @@
 use glam::{Vec2, Vec3A, Vec4};
 
-pub type Pos4 = Vec4;
-pub type Pos3 = Vec3A;
-pub type Pos2 = Vec2;
-pub type Face = (usize, usize, usize);
+pub(crate) type Pos4 = Vec4;
+pub(crate) type Pos3 = Vec3A;
+pub(crate) type Pos2 = Vec2;
+pub(crate) type Face = (usize, usize, usize);
