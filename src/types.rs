4e71819b4d41dd1f65063bd5f656ad34964d9550
ethan <ethansoya@gmail.com>
2026-06-07 23:10:24 +0200

    refactor: remove unused types

--- src/types.rs
+++ src/types.rs
@@ -1,6 +1,4 @@
-use glam::{Vec2, Vec3A, Vec4};
+use glam::{Vec2, Vec3A};
 
-pub(crate) type Pos4 = Vec4;
 pub(crate) type Pos3 = Vec3A;
 pub(crate) type Pos2 = Vec2;
-pub(crate) type Face = (usize, usize, usize);
