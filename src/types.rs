1715d94a5b99a64447018c928abc26ac4304e5f9
ethan <ethansoya@gmail.com>
2026-06-07 22:31:28 +0200

    Revert "Symbol(clack:cancel)"
    This reverts commit adf273dad59e39233e5f8ee4ee230480163414a0.

--- src/types.rs
+++ src/types.rs
@@ -1,4 +1,6 @@
-use glam::{Vec2, Vec3A};
+use glam::{Vec2, Vec3A, Vec4};
 
+pub(crate) type Pos4 = Vec4;
 pub(crate) type Pos3 = Vec3A;
 pub(crate) type Pos2 = Vec2;
+pub(crate) type Face = (usize, usize, usize);
