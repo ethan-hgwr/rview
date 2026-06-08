1f5a2740dbe0b2648609bb8cd988aec4db3c3e10
ethan <ethansoya@gmail.com>
2026-06-09 00:24:38 +0200

    refactor: radians -> degrees

--- src/cli/command.rs
+++ src/cli/command.rs
@@ -4,11 +4,11 @@ use crate::{CameraState, motion::turntable::Turntable};
 
 #[derive(clap::Args, Clone)]
 pub(crate) struct CameraArgs {
-    /// Initial yaw of the camera in radians (might get overriden depending on the choosen mode)
+    /// Initial yaw of the camera in degrees (might get overriden depending on the choosen mode)
     #[arg(short, long, default_value_t = CameraState::DEFAULT_YAW)]
     pub(crate) yaw: f32,
 
-    /// Initial pitch of the camera in radians (might get overriden depending on the choosen mode)
+    /// Initial pitch of the camera in degrees (might get overriden depending on the choosen mode)
     #[arg(short, long, default_value_t = CameraState::DEFAULT_PITCH)]
     pub(crate) pitch: f32,
 
