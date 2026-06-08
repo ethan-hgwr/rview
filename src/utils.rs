9b8a2e41c5025e798ac2d7e605c5634a7ec84954
ethan <ethansoya@gmail.com>
2026-06-09 00:35:28 +0200

    fix: suppress clippy warning

--- src/utils.rs
+++ src/utils.rs
@@ -1,14 +1,16 @@
+pub(crate) fn validate(name: &str, val: f32, min: f32, max: f32) -> anyhow::Result<()> {
+    if (min..=max).contains(&val) {
+        Ok(())
+    } else {
+        Err(anyhow::anyhow!(
+            "{name} must be between {min} and {max}, got {val}"
+        ))
+    }
+}
+
 macro_rules! validate_range {
     ($val:expr, $min:expr, $max:expr) => {
-        if !($val >= $min && $val <= $max) {
-            return Err(anyhow::anyhow!(
-                "{} must be between {} and {}, got {}",
-                stringify!($val),
-                $min,
-                $max,
-                $val
-            ));
-        }
+        $crate::utils::validate(stringify!($val), $val, $min, $max)?
     };
 }
 
